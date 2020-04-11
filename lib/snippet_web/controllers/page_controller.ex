90ef241791bdbf5b8b9141bb8d1a70d1399e7e99
Lucas Stettner <lebra448@gmail.com>
2020-04-11 18:41:48 -0400

    add navbar

--- lib/snippet_web/controllers/page_controller.ex
+++ lib/snippet_web/controllers/page_controller.ex
@@ -2,9 +2,6 @@ defmodule SnippetWeb.PageController do
   use SnippetWeb, :controller
 
   def index(conn, _params) do
-    conn
-    |> put_flash(:info, "Hello World!")
-    |> put_flash(:error, "Hello World!")
-    |> render("index.html")
+    render(conn, "index.html")
   end
 end
